7c66ff56797e7e3ad7b1d8ac01d688038d57915f
Zack Edicola <42786624+ZetaTauEpsilon@users.noreply.github.com>
2023-11-16 12:15:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from server import socketio, app
 
 # Run the Flask application
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host='0.0.0.0', port='80')
+    socketio.run(app, debug=True, host='0.0.0.0', port='8080')
     print("Started Server")
