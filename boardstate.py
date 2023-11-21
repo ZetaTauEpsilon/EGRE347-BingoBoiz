18bd84832c5a89f9d96f19471ab5f2f1beb45c4d
Mooshroom14 <>
2023-11-21 17:12:51 -0500

    Game Logic for switching States

--- boardstate.py
+++ boardstate.py
@@ -5,8 +5,11 @@ class BoardState:
         self.state
         self.contents
 
-    def updateState(self) -> bool:
-        pass
+    def updateState(self, row, col) -> bool:
+        if(self.state[row][col] == False):
+            self.state[row][col] = True
+        else:
+            self.state[row][col] = False
     """Inputs: Row and Column of Tile"""
     # Change the state of the tile at given coordinates
     # Based on the current state of the tile
