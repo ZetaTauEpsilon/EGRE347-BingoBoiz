50edc543bb10631ab8e9566130c4ca97c2e65eaa
Mooshroom14 <>
2023-11-29 14:10:07 -0500

    Wrote createBoard()
    Also updated function UpdateState() to a ternary operator.

--- boardstate.py
+++ boardstate.py
@@ -3,13 +3,10 @@ class BoardState:
     
     def __init__(self):
         self.state
-        self.contents
+     #   self.contents
 
     def updateState(self, row, col) -> bool:
-        if(self.state[row][col] == False):
-            self.state[row][col] = True
-        else:
-            self.state[row][col] = False
+        self.state[row][col] = True if self.state[row][col] == False else False
     """Inputs: Row and Column of Tile"""
     # Change the state of the tile at given coordinates
     # Based on the current state of the tile
--- gamemanager.py
+++ gamemanager.py
@@ -1,5 +1,7 @@
 from lobby import Lobby
 from json import JSON
+import random
+from boardstate import BoardState
 
 
 class GameManager():
@@ -7,13 +9,33 @@ class GameManager():
     def __init__():
         pass
 
-    def makeBoard(Lobby: Lobby) -> JSON:
-        pass
+    def makeBoard(Lobby: Lobby, tileSet, isFreeEnabled, boardSize) -> JSON:
+        board = [[0 for i in range(boardSize)] for j in range(boardSize)]
+        state = [[False for i in range(boardSize)] for j in range(boardSize)]
+        tileSampleSize = boardSize*boardSize
+        availableTiles = random.sample(tileSet, tileSampleSize)
+        tileCt = 0
+        for row in boardSize:
+            for col in boardSize:
+                board[row][col] = availableTiles[tileCt]
         """Inputs: TileSet, IsFreeEnabled"""
         # Create a 2D array from a list of tiles depending on the set
+            # Loop that selects a random tile from the set and puts it into the 
         # Set the middle tile to free if its enabled
+        if(isFreeEnabled):
+            isEven = True if boardSize % 2 == 0 else False
+            if(isEven):
+                # choose random tile
+                randCol = random.randrange(boardSize-1)
+                randRow = random.randrange(boardSize-1)
+                board[randRow][randCol] = "Free"
+                state[randRow][randCol] = True
+            else:
+                center = round(boardSize/2) + 1
+                board[center][center] = "Free"
+                state[center][center] = True
 
-    def evaluateWin(BoardState: BoardState) -> bool:
+    def evaluateWin(BoardState: BoardState, tileX, tileY) -> bool:
         pass
         """Inputs: Tile Coordinates"""
         # Run this function every time a tile state is changed
